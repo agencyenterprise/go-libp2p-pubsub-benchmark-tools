50a4fab076f28bc1fb04b45d62efbb2c8451f730
adam <ahanna@alumni.mines.edu>
2019-09-19 10:53:57 -0700

    using viper for config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,26 +1,20 @@
 package config
 
 import (
-	"encoding/json"
-	"io/ioutil"
-
 	"github.com/agencyenterprise/gossip-host/pkg/logger"
 )
 
 // Load reads the passed config file location and parses it into a config struct.
-func Load(confLoc string) (*Config, error) {
+func Load(confLoc, listens, peers string) (*Config, error) {
 	var conf Config
 
-	file, err := ioutil.ReadFile(confLoc)
-	if err != nil {
-		logger.Errorf("err reading configuration file:%s\n%v", confLoc, err)
+	if err := parseConfigFile(&conf, confLoc); err != nil {
+		logger.Errorf("err parsing config file:\n%v", err)
 		return nil, err
 	}
 
-	if err = json.Unmarshal([]byte(file), &conf); err != nil {
-		logger.Errorf("err unmarshaling config\n%v", err)
-		return nil, err
-	}
+	parseListens(&conf, listens)
+	parsePeers(&conf, peers)
 
 	return &conf, nil
 }
