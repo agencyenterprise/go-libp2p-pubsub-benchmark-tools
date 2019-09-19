87834184a4b3fe9538683e4c2323167a57b22c68
adam <ahanna@alumni.mines.edu>
2019-09-19 10:28:44 -0700

    adding viper

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,26 +1,40 @@
 package config
 
 import (
-	"encoding/json"
-	"io/ioutil"
-
 	"github.com/agencyenterprise/gossip-host/pkg/logger"
+
+	"github.com/spf13/viper"
 )
 
 // Load reads the passed config file location and parses it into a config struct.
-func Load(confLoc string) (*Config, error) {
+func Load(confLoc, listens, peers string) (*Config, error) {
 	var conf Config
 
-	file, err := ioutil.ReadFile(confLoc)
-	if err != nil {
+	viper.SetConfigName(trimExtension(confLoc))
+
+	if err := viper.ReadInConfig(); err != nil {
 		logger.Errorf("err reading configuration file:%s\n%v", confLoc, err)
 		return nil, err
 	}
 
-	if err = json.Unmarshal([]byte(file), &conf); err != nil {
+	if err := viper.Unmarshal(&conf); err != nil {
 		logger.Errorf("err unmarshaling config\n%v", err)
 		return nil, err
 	}
+	logger.Infof("config: %v", conf)
+
+	/*
+		file, err := ioutil.ReadFile(confLoc)
+		if err != nil {
+			logger.Errorf("err reading configuration file:%s\n%v", confLoc, err)
+			return nil, err
+		}
+
+		if err = json.Unmarshal([]byte(file), &conf); err != nil {
+			logger.Errorf("err unmarshaling config\n%v", err)
+			return nil, err
+		}
+	*/
 
 	return &conf, nil
 }
