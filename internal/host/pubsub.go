d335868cca59635874580131d255c0a61ab6567a
adam <ahanna@alumni.mines.edu>
2019-09-24 11:27:37 -0700

    changes to pubsub log

--- internal/host/pubsub.go
+++ internal/host/pubsub.go
@@ -2,9 +2,11 @@ package host
 
 import (
 	"context"
+	"encoding/binary"
 	"time"
 
 	"github.com/agencyenterprise/gossip-host/pkg/logger"
+	"github.com/davecgh/go-spew/spew"
 
 	pb "github.com/agencyenterprise/gossip-host/internal/pb/publisher"
 	peer "github.com/libp2p/go-libp2p-peer"
@@ -19,14 +21,17 @@ func pubsubHandler(ctx context.Context, hostID peer.ID, sub *pubsub.Subscription
 			continue
 		}
 
+		// TODO: fix this. It isn't working
 		msg := &pb.Message{}
 		if err = nxt.Unmarshal(nxt.Data); err != nil {
 			logger.Errorf("err unmarshaling next message:\n%v", err)
 			continue
 		}
+		spew.Dump(msg)
 
 		// TODO: how to increment sequence before sending out?
-		logger.Infof("Pubsub message received: %v,%v,%v,%d,%d", hostID, nxt.GetFrom(), msg.Id, time.Now().UnixNano(), msg.Sequence)
+		// note: what is nxt.GetFrom()? It doesn't seem to be the sender id but it doesn't match the original host peer.Id?
+		logger.Infof("Pubsub message received: %v,%v,%v,%v,%d,%d", hostID, nxt.GetFrom(), msg.GetId(), binary.BigEndian.Uint64(nxt.GetSeqno()), time.Now().UnixNano(), msg.Sequence)
 	}
 }
 
