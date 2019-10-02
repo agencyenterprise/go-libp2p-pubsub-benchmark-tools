79675dc55b95c76061649bba55e23020d34afc26
Adam Hanna <ahanna@alumni.mines.edu>
2019-10-02 16:25:53 +0000

    minor changes

--- pkg/subnet/peertopology/full/full.go
+++ pkg/subnet/peertopology/full/full.go
@@ -5,16 +5,12 @@ import (
 	"github.com/agencyenterprise/gossip-host/pkg/logger"
 )
 
-// Build connects the hosts using the linear topology
+// Build connects the hosts using the full topology
 func Build(hosts []*host.Host) error {
 	var err error
 
 	for i := range hosts {
-		for j := range hosts {
-			if i == j {
-				continue
-			}
-
+		for j := i + 1; j < len(hosts); j++ {
 			if err = hosts[i].Connect(hosts[j].IFPSAddresses()); err != nil {
 				logger.Errorf("err connecting %s with %s:\n%v", hosts[i].ID(), hosts[j].ID(), err)
 				return err
